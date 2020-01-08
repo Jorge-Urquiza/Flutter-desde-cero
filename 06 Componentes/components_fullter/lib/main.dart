6719a72fa8edf6f43e785de2b999d8ccde2242a4
Jorge Luis <jorgestudio2017@gmail.com>
2020-01-08 02:32:08 -0400

    Avanzando en el proyecto de la parte de componentes

---
+++ 06 Componentes/components_fullter/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:components_fullter/src/home_temp.dart';
+import 'package:flutter/material.dart';
+void main()=> runApp(MyApp());
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Componentes",
+      home:  HomePageTemp()
+    );
+  }
+
+}
